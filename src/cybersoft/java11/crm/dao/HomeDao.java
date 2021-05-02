4ca5fb399fcd1c3e9c1a77707bb5aefb9048752c
ThaoHien1998 <tranthaohien.1998@gmail.com>
2021-05-02 11:20:00 +0700

    Connect to Database

--- src/cybersoft/java11/crm/dao/HomeDao.java
+++ src/cybersoft/java11/crm/dao/HomeDao.java
@@ -1,5 +1,37 @@
 package cybersoft.java11.crm.dao;
 
-public class HomeDao {
+// dao : Database Access Object
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import cybersoft.java11.crm.config.MySQLConnection;
+
+public class HomeDao {
+	public boolean testConnection() {
+		Connection connection = MySQLConnection.getConnection();
+		if (connection == null) {
+			return false;
+		}
+		return true;
+	}
+	public boolean checkHealth() {
+		boolean result = false;
+		Connection connection = MySQLConnection.getConnection();
+		if (connection == null) 
+			return result;
+		
+		String test = "select 1 from Dual";
+		Statement statement;
+		try {
+			statement = connection.createStatement();
+			result = statement.execute(test);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;	
+	}
 }
--- src/cybersoft/java11/crm/servlet/HomeServlet.java
+++ src/cybersoft/java11/crm/servlet/HomeServlet.java
@@ -1,5 +1,34 @@
 package cybersoft.java11.crm.servlet;
 
-public class HomeServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cybersoft.java11.crm.biz.HomeBiz;
+
+@WebServlet(name="homeServlet", urlPatterns = {"/health"})
+public class HomeServlet extends HttpServlet{
+	private HomeBiz biz;
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		biz = new HomeBiz();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		boolean databaseCheckResult = biz.checkHealth();
+		if (databaseCheckResult)
+			resp.getWriter().append("Connection to databae has been made successfully!");
+		else
+			resp.getWriter().append("Connection to databae has been made unsuccessfully!");
+	}
+	
 }
